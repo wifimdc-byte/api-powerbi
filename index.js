7ede25e3a05c30b4d997e799eeb7223b16370eb5
wifimdc-byte <wifimdc@gmail.com>
2026-06-17 12:07:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const app = express();
 
 const TOKEN = process.env.TOKEN;
 
+// função pra converter data
+function formatarData(data) {
+  const [ano, mes, dia] = data.split("-");
+  return `${dia}-${mes}-${ano}`;
+}
+
 app.get("/vendas", async (req, res) => {
   try {
     const { ini, fim } = req.query;
 
-    const url = `https://casasdamamae.innovaro.com.br/api/izi/v1/vendas?ini=${ini}&fim=${fim}`;
+    if (!ini || !fim) {
+      return res.status(400).json({ erro: "Informe 'ini' e 'fim'" });
+    }
+
+    const iniFormatado = formatarData(ini);
+    const fimFormatado = formatarData(fim);
+
+    const url = `https://casasdamamae.innovaro.com.br/api/izi/v1/vendas?ini=${iniFormatado}&fim=${fimFormatado}`;
 
     const response = await fetch(url, {
       headers: {
